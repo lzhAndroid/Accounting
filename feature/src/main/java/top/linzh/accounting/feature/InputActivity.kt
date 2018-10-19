b381849e6aa89e733cf42f0c04164fc7ba76107b
linzh <linzh@lbschat.com>
2018-10-19 10:00:50 +0800

    init project

---
+++ feature/src/main/java/top/linzh/accounting/feature/InputActivity.kt
@@ -0,0 +1,12 @@
+package top.linzh.accounting.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
